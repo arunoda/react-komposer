3feeb067d4450acc5c0f8fceec1d3f9f52358247
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-01-02 17:01:28 +0530

    Move browser specific code to a new file

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,7 +1,8 @@
 echo "> Start transpiling ES2015"
 echo ""
-./node_modules/.bin/babel --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
-./node_modules/.bin/browserify --debug -t [ exposify --expose [ --react React ] ] ./dist/index.js > ./dist/browser.js
-cat ./dist/browser.js | ./node_modules/.bin/uglifyjs -c > ./dist/browser.min.js
+babel --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
+cd dist
+browserify --debug -t [ exposify --expose [ --react React ] ] ./window_bind.js > ./browser.js
+cat ./browser.js | uglifyjs -c > ./browser.min.js
 echo ""
 echo "> Complete transpiling ES2015"
