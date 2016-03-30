b7056f73bde844185e14af2fc8d01f3e3d58c4a4
Andrew Shini <superandrew213@gmail.com>
2016-03-30 14:08:47 +1000

    Ignore `require()` statements that don't resolve to anything for peer dependencies

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -2,7 +2,7 @@ echo "> Start transpiling ES2015"
 echo ""
 babel --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
 cd dist
-browserify --debug -t [ exposify --expose [ --react React ] ] ./window_bind.js > ./browser.js
+browserify --debug --ignore-missing -t [ exposify --expose [ --react React ] ] ./window_bind.js > ./browser.js
 cat ./browser.js | uglifyjs -c > ./browser.min.js
 echo ""
 echo "> Complete transpiling ES2015"
