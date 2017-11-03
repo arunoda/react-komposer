7f2fa2414404fc48b14e2ab29e9c4060f194d35f
Colin Dekker <colin.dekker@outlook.com>
2017-11-03 01:42:59 +0100

    chore: added cross-env development, enabled react 16

--- dist/index.js
+++ dist/index.js
@@ -1,43 +1,25 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.composeAll = exports.compose = exports.stub = exports.setStubbingMode = undefined;
-
-var _extends2 = require('babel-runtime/helpers/extends');
-
-var _extends3 = _interopRequireDefault(_extends2);
-
-exports.setDefaults = setDefaults;
-exports.merge = merge;
-
-var _reactStubber = require('react-stubber');
-
-var _compose2 = require('./compose');
-
-var _compose3 = _interopRequireDefault(_compose2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+import _Object$assign from 'babel-runtime/core-js/object/assign';
 /* eslint import/prefer-default-export: 0 */
-var setStubbingMode = exports.setStubbingMode = _reactStubber.setStubbingMode;
-var stub = exports.stub = _reactStubber.stub;
-var compose = exports.compose = _compose3.default;
+import { setStubbingMode as _setStubbingMode, stub as _stub } from 'react-stubber';
+import _compose from './compose';
 
-function setDefaults() {
-  var mainOptions = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+export var setStubbingMode = _setStubbingMode;
+export var stub = _stub;
+export var compose = _compose;
+
+export function setDefaults() {
+  var mainOptions = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
   return function (dataLoader) {
-    var otherOptions = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+    var otherOptions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    var options = (0, _extends3.default)({}, mainOptions, otherOptions);
+    var options = _Object$assign({}, mainOptions, otherOptions);
 
-    return (0, _compose3.default)(dataLoader, options);
+    return _compose(dataLoader, options);
   };
 }
 
-function merge() {
+export function merge() {
   for (var _len = arguments.length, enhancers = Array(_len), _key = 0; _key < _len; _key++) {
     enhancers[_key] = arguments[_key];
   }
@@ -50,4 +32,4 @@ function merge() {
   };
 }
 
-var composeAll = exports.composeAll = merge;
+export var composeAll = merge;
